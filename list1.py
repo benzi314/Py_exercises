6987c881941b2138c938170d81855633fae18f0e
benzi314 <bijoychaudhary01@outlook.com>
2018-08-09 16:27:13 +0530

    This program is for practicing lists

--- list1.py
+++ list1.py
@@ -4,18 +4,20 @@
 #and write a program that prints out all the elements of the list that are less than 5.
 a = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = len(a)
+new1 = []
 print ("Number of elements in a is %d" %b)
 for i in range (0,b):
     if a[i]<5:
-        a.append(a[i])
-        print(a[i])
+        new1.append(a[i])
+print(new1)
 #below codes requires user to enter the list
 new =[]
 for i in range(0,10):
     x = int(input("enter the %d th element of the list:" %i))
     new.append(x)
 print(new)
+new2 = []
 for i in range(0,len(new)):
     if new[i]<5:
-        new.append(new[i])
-        print(new[i])
+        new2.append(new[i])
+print(new2)
